7506cd46f6c466df947a53d2f689ce60fd177d13
Jonah Casebeer <jonah.casebeer@gmail.com>
2016-09-24 14:33:08 -0500

    2 events free time checking solution

--- event_prototype.py
+++ event_prototype.py
@@ -5,9 +5,14 @@ class event(object):
         self.end_time = input_end_time
         self.start_time = input_start_time
 
-    def event_starts(hour, minute):
+    def event_starts(self,hour, minute):
         if hour == self.start_time.hour and minute == self.start_time.minute:
             return True
+        return False
+    def event_ends(self,hour, minute):
+        if hour == self.end_time.hour and minute == self.end_time.minute:
+            return True
+        return False
 
 data1 = {
   "timeMax": "2016-09-25T17:36:12.000Z",
@@ -43,3 +48,37 @@ data2 = {
 
 event1 = event(datetime.datetime.strptime(data1['calendars']['primary']['busy'][0]['end'], "%Y-%m-%dT%H:%M:%Sz"), datetime.datetime.strptime(data1['calendars']['primary']['busy'][0]['start'], "%Y-%m-%dT%H:%M:%Sz"))
 event2 = event(datetime.datetime.strptime(data2['calendars']['primary']['busy'][0]['end'], "%Y-%m-%dT%H:%M:%Sz"), datetime.datetime.strptime(data2['calendars']['primary']['busy'][0]['start'], "%Y-%m-%dT%H:%M:%Sz"))
+
+
+def checkTime(event1, event2):
+    event1_count = 0
+    event2_count = 0
+    free_zones = []
+    free_zone_count = 0
+    free = False
+    for hour in range(24):
+        for minute in range(60):
+            if(event1.event_starts(hour,minute)):
+                event1_count +=1
+            elif (event1.event_ends(hour,minute)):
+                event1_count -= 1
+
+            if (event2.event_starts(hour,minute)):
+                event2_count += 1
+            elif (event2.event_ends(hour,minute)):
+                event2_count -= 1
+
+            if(event1_count==event2_count==0 and not free):
+                free_zones.append([(hour,minute)])
+                free = True
+            elif(not event1_count==event2_count and free):
+                free_zones[free_zone_count].append((hour,minute))
+                free = False
+                free_zone_count+=1
+    if(len(free_zones)%2 != 0):
+      free_zones[free_zone_count].append((23,59))
+    print free_zones
+    return free_zones
+
+checkTime(event1,event2)
+
